455993935cea9e81d4e06098081527a0ee7d8d6a
康  查 <956468883@qq.com>
2018-06-09 17:13:48 +0800

    xigai

--- model/index.js
+++ model/index.js
@@ -1,20 +1,20 @@
 export * from './user'
 
 export default class Model {
-  findOneById (Model, { id }) {
+  findOneById(Model, { id }) {
     return Model.findOne({ _id: id })
   }
-  
-  findOneByOps (Model, { ops }) {
+
+  findOneByOps(Model, { ops }) {
     return Model.findOne(ops)
   }
-  
-  save (Model, { doc }) {
+
+  save(Model, { doc }) {
     let instance = new Model(doc)
     return instance.save()
   }
-  
-  updateByOps (Model, { ops, sets }) {
+
+  updateByOps(Model, { ops, sets }) {
     return Model.update(ops, sets)
   }
 }
