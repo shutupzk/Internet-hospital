a18919d8000b9a2253b5b8b1f46da404464baca7
ruan <894159898@qq.com>
2018-06-09 20:13:49 +0800

    分页

--- model/index.js
+++ model/index.js
@@ -29,7 +29,7 @@ class Model {
     skip = skip || 0
     if (sort) sort = { created_at: -1 }
     let total = await Model.count(ops)
-    let items = Model.find(ops)
+    let items = await Model.find(ops)
       .sort(sort)
       .skip(skip)
       .limit(limit)
