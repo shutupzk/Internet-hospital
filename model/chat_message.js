28b28a62ba780ec62df058a42bffb2e22bfbde63
ruan <894159898@qq.com>
2018-06-09 18:59:43 +0800

    消息

--- model/chat_message.js
+++ model/chat_message.js
@@ -12,7 +12,12 @@ const schema = new Schema(
     type: String, // 01: 图文，02： 处方, 03: 检查 04 检验， 05：结束回话, 06: 咨询申明, 07, 门诊缴费, 08: 住院登记 09: 预约挂号， 10： 加号消息  21:拒绝接单 22:服务评价消息 23:售后评价消息 24: 意见反馈意见
     text: String, // 文字
     image: String, // 图片
-    audio: String // 语音
+    audio: String, // 语音
+    direction: String, // user->doctor, doctor->user, system->user, system->doctor
+    isRetract: Boolean, // 是否撤回
+    prescriptionId: ObjectId, // 检验id
+    examId: ObjectId, // 检查id
+    laboraId: ObjectId // 处方id
   },
   {
     versionKey: false // 这个就是处理掉自动插入文档的__v这个属性
