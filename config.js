47e965daa56528974502fd2211a77ca8d090a908
康  查 <956468883@qq.com>
2018-06-09 17:11:03 +0800

    修改数据库配置

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const PORT = '3000'
 const DB = {
-  mongoUrl: 'mongodb://localhost:27017/internet_hospital',
+  mongoUrl: 'mongodb://192.168.0.5:27017/internet_hospital',
   user: 'internet_hospital',
   pass: 'internet_hospital'
 }
