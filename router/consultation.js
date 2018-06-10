ba72d7193aca906d5669864b79e276aa6c81b73d
田先耀 <xianyao9958@163.com>
2018-06-10 16:24:06 +0800

    修改

--- router/consultation.js
+++ router/consultation.js
@@ -5,7 +5,6 @@ const router = express.Router()
 
 router.all('/create', createConsultation)
 router.all('/payment/create', createConsultationPayment)
-router.all('/create', createConsultation)
 router.all('/update', updateConsultation)
 router.all('/list', consultationList)
 router.all('/chatmessage', consultationChat)
