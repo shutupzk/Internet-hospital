98aa485ba51920c40469ac0fcbdbac573f4ff330
ruan <894159898@qq.com>
2018-06-10 14:12:23 +0800

    快捷回复

--- controller/quickReply.js
+++ controller/quickReply.js
@@ -1,9 +1,11 @@
-import Model, { QuickReply } from '../model'
+import Model, { QuickReply, Doctor } from '../model'
 import result from './result'
 
 export const quickReplyCreate = async (req, res) => {
   const { doctorId, title, content } = req.body
   if (!doctorId || !title || !content) return result.failed(res, '缺少参数')
+  let doctor = await Doctor.findById(doctorId)
+  if (!doctor) return result.failed(res, '未找到指定的医生')
   let data = await QuickReply.create({ doctorId, title, content })
   return result.success(res, data)
 }
@@ -11,7 +13,7 @@ export const quickReplyCreate = async (req, res) => {
 export const quickReplyDelete = async (req, res) => {
   const { id } = req.body
   if (!id) return result.failed(res, '缺少参数')
-  let data = await QuickReply.create()
+  let data = await QuickReply.deleteOne({_id: id})
   return result.success(res, data)
 }
 
@@ -20,6 +22,6 @@ export const quickReplyList = async (req, res) => {
   let ops = {}
   if (doctorId) ops.doctorId = doctorId
   if (keyword) ops['$or'] = [{ title: { $regex: keyword, $options: 'i' } }, { content: { $regex: keyword, $options: 'i' } }]
-  let data = await Model.findByOpsWithPage({ ops, limit, skip })
+  let data = await Model.findByOpsWithPage(QuickReply, { ops, limit, skip })
   return result.success(res, data)
 }
