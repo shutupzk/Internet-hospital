5bedf90a8da65ff295219dd5b9388f18f1d01469
田先耀 <xianyao9958@163.com>
2018-06-13 00:00:16 +0800

    修改

---
+++ model/east_prescription_item.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+
+/**
+ * 中药处方item
+ * @type {Schema}
+ */
+const schema = new Schema(
+  {
+    east_prescriptionId: { type: ObjectId, ref: 'east_prescription' }, // 中药处方id
+    drugId: { type: ObjectId, ref: 'drug' }, // 中药id
+    amount: Number, // 数量
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+    deleted_at: { type: Date }
+  },
+  {
+    versionKey: false // 这个就是处理掉自动插入文档的__v这个属性
+  }
+)
+
+const EastPrescriptionItem = mongoose.model('east_prescription_item', schema, 'east_prescription_item')
+
+export { EastPrescriptionItem }
