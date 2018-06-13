219f3374431d8e1615e716abf85d8043e31179fb
田先耀 <xianyao9958@163.com>
2018-06-13 22:05:01 +0800

    修改

--- controller/dictionary.js
+++ controller/dictionary.js
@@ -1,5 +1,20 @@
-import Model, { DiagnosisDictionary, ExaminationDictionary, ExaminationOrganDictionary, ExaminationTypeDictionary } from '../model'
+import
+Model,
+{
+  DiagnosisDictionary,
+  ExaminationDictionary,
+  ExaminationOrganDictionary,
+  ExaminationTypeDictionary,
+  Drug,
+  DrugClass,
+  DoseUnit,
+  DoseForm,
+  ManuFactory,
+  Frequency,
+  RouteOfAdministration
+} from '../model'
 import result from './result'
+import { formatArrayId, formatObjId } from '../util'
 
 export const diagnosisDictionaryCreate = async (req, res) => {
   const { name } = req.body
@@ -57,4 +72,91 @@ export const examinationTypeDictionargList = async (req, res) => {
   if (keyword) query['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
   let data = await ExaminationTypeDictionary.find(query, ['_id', 'name', 'code', 'pyCode'])
   return result.success(res, data)
+}
+
+export const drugList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { code: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const drugList = await Model.findDrugByOpsWithPage(Drug, { ops, limit, skip })
+  drugList.items = formatArrayId(drugList.items, ['drugClass'])
+  for (let item of drugList.items) {
+    if (item.drugClass) {
+      item.drugClass = formatObjId(item.drugClass)
+    }
+  }
+  return result.success(res, drugList)
+}
+
+export const drugClassList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const drugClassList = await Model.findByOpsWithPage(DrugClass, { ops, limit, skip })
+  drugClassList.items = formatArrayId(drugClassList.items)
+  return result.success(res, drugClassList)
+}
+
+export const doseUnitList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { code: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const doseUnitList = await Model.findByOpsWithPage(DoseUnit, { ops, limit, skip })
+  doseUnitList.items = formatArrayId(doseUnitList.items)
+
+  return result.success(res, doseUnitList)
+}
+
+export const doseFormList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { code: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const doseFormList = await Model.findByOpsWithPage(DoseForm, { ops, limit, skip })
+  doseFormList.items = formatArrayId(doseFormList.items)
+
+  return result.success(res, doseFormList)
+}
+
+export const manuFactoryList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { code: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const manuFactoryList = await Model.findByOpsWithPage(ManuFactory, { ops, limit, skip })
+  manuFactoryList.items = formatArrayId(manuFactoryList.items)
+
+  return result.success(res, manuFactoryList)
+}
+
+export const frequencyList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { code: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const frequencyList = await Model.findByOpsWithPage(Frequency, { ops, limit, skip })
+  frequencyList.items = formatArrayId(frequencyList.items)
+
+  return result.success(res, frequencyList)
+}
+
+export const routeAdministrationList = async (req, res) => {
+  const { keyword, skip, limit } = req.body
+  let ops = {}
+  if (keyword) {
+    ops['$or'] = [{ name: { $regex: keyword, $options: 'i' } }, { code: { $regex: keyword, $options: 'i' } }, { pyCode: { $regex: keyword, $options: 'i' } }]
+  }
+  const routeAdministrationList = await Model.findByOpsWithPage(RouteOfAdministration, { ops, limit, skip })
+  routeAdministrationList.items = formatArrayId(routeAdministrationList.items)
+
+  return result.success(res, routeAdministrationList)
 }
