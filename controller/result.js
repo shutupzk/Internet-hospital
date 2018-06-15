7ea2bb8efe52548967bdb5842a4132fefd3119ed
康  查 <956468883@qq.com>
2018-06-15 23:00:04 +0800

    xiugai

--- controller/result.js
+++ controller/result.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 var Result = {}
 /**
  * 请求成功，但是操作失败，json数据
@@ -23,9 +24,29 @@ Result.failed = function(res, msg, code) {
 Result.success = function(res, data) {
   return res.json({
     code: 200,
-    data: data,
+    data: formatDate(data),
     msg: '操作成功'
   })
+}
+
+function formatDate(data) {
+  if (data instanceof Array) {
+    console.log('iii')
+    for (let obj of data) {
+      formatDate(obj)
+    }
+  } else if (data instanceof Object) {
+    for (let key in data) {
+      if (data[key] instanceof Array || data[key] instanceof Object) {
+        formatDate(data[key])
+      } else {
+        if (data[key] && isNaN(data[key]) && !isNaN(Date.parse(data[key]))) {
+          data[key] = moment(data[key]).format('YYYY-MM-DD HH:mm:ss')
+        }
+      }
+    }
+  }
+  return data
 }
 
 export default Result
